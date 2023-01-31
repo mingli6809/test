39af15df4355585d73121060b4df21e685f4dbbf
mingli <greenvideo6809@gmail.com>
2023-01-31 07:57:18 -0800

    first

---
+++ WebApplication1/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using MvcApplication1.Models;
+using System.Data.Entity;
+
+
+namespace WebApplication1
+{
+    public class AppContext : DbContext
+    {
+        public AppContext()
+            : base("DefaultConnection")
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
